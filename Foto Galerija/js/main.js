02ab57ccf85bf13ef1a9eea941a64bc280775acc
nickola23 <nikolamilanovic2305@gmail.com>
2024-03-26 18:29:43 +0100

    galerija nije jos zavrsena

--- Foto Galerija/js/main.js
+++ Foto Galerija/js/main.js
@@ -1,6 +1,17 @@
 const navImages = document.querySelectorAll('.navImg');
 const mainContImg = document.querySelector('.mainContImg');
 const mainContText = document.querySelector('.mainContText');
+const opisi = "";
+const centralniOpis = "";
+
+fetch('./txt/opisiSlika.txt')
+    .then(response => response.text())
+    .then(text => {
+    opisi = text.split('\n\n');
+    centralniOpis = opisi[0];
+    mainContText.textContent = centralniOpis;
+})
+.catch(error => console.error('Грешка при учитавању описа слика:', error));
 
 navImages.forEach(img => {
     img.addEventListener('click', () => {
