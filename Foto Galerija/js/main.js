13473fdd85245c83c37c31d4c7ac93bbd66ad681
nickola23 <nikolamilanovic2305@gmail.com>
2024-04-10 12:57:25 +0100

    popravljeno ucitavanje opisa i dodat biznis plan

--- Foto Galerija/js/main.js
+++ Foto Galerija/js/main.js
@@ -1,23 +1,22 @@
 const navImages = document.querySelectorAll('.navImg');
 const mainContImg = document.querySelector('.mainContImg');
 const mainContText = document.querySelector('.mainContText');
-const opisi = "";
+const opisi = [];
 const centralniOpis = "";
-
-fetch('./txt/opisiSlika.txt')
-    .then(response => response.text())
-    .then(text => {
-    opisi = text.split('\n\n');
-    centralniOpis = opisi[0];
-    mainContText.textContent = centralniOpis;
-})
-.catch(error => console.error('Грешка при учитавању описа слика:', error));
 
 navImages.forEach(img => {
     img.addEventListener('click', () => {
     const imgSrc = img.getAttribute('src');
-    const imgAlt = img.getAttribute('alt');
-    const newImgSrc = "." + imgSrc;
+    const newImgSrc = imgSrc;
     mainContImg.style.backgroundImage = "url(" + newImgSrc + ")";
-    mainContText.innerHTML = imgAlt;
+    mainContText.textContent = opisi[3];
 })});
+
+fetch('../txt/opisiSlika.txt')
+    .then(response => response.text())
+    .then(text => {
+    opisi = text.split('\n');
+    centralniOpis = opisi[0];
+    mainContText.textContent = centralniOpis;
+})
+.catch(error => console.error('Грешка при учитавању описа слика:', error));
